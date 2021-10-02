7d81058a00e96db06a8ca24193d6c748d620a584
Sprial404 <1sprial404@gmail.com>
2021-10-02 18:04:48 +0800

    Split clamp and abs functions into util header file

--- Game.cpp
+++ Game.cpp
@@ -2,15 +2,7 @@
 
 namespace pong {
 
-template<typename T>
-inline const T clamp(const T& value, const T& min, const T& max) {
-    return value < min ? min : (value > max ? max : value);
-}
-
-template<typename T>
-inline const T abs(const T& x) {
-    return (x < 0) ? -x : x;
-}
+#include "Util.h"
 
 Game::Game() {
     m_window   = nullptr;
---
+++ Util.h
@@ -0,0 +1,16 @@
+#ifndef PONG_UTIL_H
+#define PONG_UTIL_H
+
+namespace pong {
+template<typename T>
+inline const T clamp(const T& value, const T& min, const T& max) {
+    return value < min ? min : (value > max ? max : value);
+}
+
+template<typename T>
+inline const T abs(const T& x) {
+    return (x < 0) ? -x : x;
+}
+} // namespace pong
+
+#endif
